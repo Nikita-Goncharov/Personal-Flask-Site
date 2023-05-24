82f5ffe4838205d3f79813fce1bebcd94583cc65
Nikita Goncharov <nikitagoncarov657@gmail.com>
2023-05-24 15:17:36 +0300

    delete file with token

--- flask_/static/js/gitAPIToken.js
+++
@@ -1,3 +0,0 @@
-const GITHUBAPI_TOKEN = "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"
-const GITHUBAPI_URL = "https://api.github.com/users/Nikita-Goncharov/repos"
-export {GITHUBAPI_TOKEN, GITHUBAPI_URL}
